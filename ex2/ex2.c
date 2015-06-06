f302c2acdcd141035a345415cdff53c6bf4edbcc
arijit <arijit@example.com>
2015-06-06 12:48:22 +0530

    Add ex2

---
+++ ex2/ex2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+	/* playing with numbers in printing */
+	int height = 72;
+	int age = 10;
+
+	printf("I am %d years old\n", age);
+	printf("I'm %d inches tall\n", height);
+	
+	return 0;
+}
